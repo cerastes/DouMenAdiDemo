e65ddc1be52f82bc220e567bbfcb2e2bf001e8d8
cerastes <chn_jin@163.com>
2016-03-21 11:30:25 +0800

    Initial Commit

---
+++ DouMenAdiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouMenAdiDemo
+//
+//  Created by JOB on 16/3/21.
+//  Copyright © 2016年 mobi.wonders.ios.apps.cswhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouMenAdiDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DouMenAdiDemo
+//
+//  Created by JOB on 16/3/21.
+//  Copyright © 2016年 mobi.wonders.ios.apps.cswhy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
